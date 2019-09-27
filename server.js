3c23e529d0ad7f9c0df858bdc5e0666c5eaf1816
tbiaglow <tbiaglow@gmail.com>
2019-09-27 19:39:08 -0400

    attempting to debug heroku routing

--- server.js
+++ server.js
@@ -10,10 +10,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("/*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
-  });
+  // app.use(express.static("client/build"));
+  // app.get("/*", function(req, res) {
+  //   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  app.use('/static', express.static(path.join(__dirname, 'client/build')));
+
 }
 // Add routes, both API and view
 app.use(routes);
