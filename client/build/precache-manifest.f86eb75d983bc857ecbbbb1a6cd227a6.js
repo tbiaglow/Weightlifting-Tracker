dc951e71d11be524187ccb3a4ef477e25bfbd593
tbiaglow <tbiaglow@gmail.com>
2019-09-27 18:01:09 -0400

    setting up heroku

---
+++ client/build/precache-manifest.f86eb75d983bc857ecbbbb1a6cd227a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dab583b4bacf55d0ac4d",
+    "url": "/static/js/main.3792afcb.chunk.js"
+  },
+  {
+    "revision": "8f477e63a7a4eb199c58",
+    "url": "/static/js/2.6c6e80ea.chunk.js"
+  },
+  {
+    "revision": "dab583b4bacf55d0ac4d",
+    "url": "/static/css/main.d22313db.chunk.css"
+  },
+  {
+    "revision": "ed4922a94d188a81e342d746d61265d6",
+    "url": "/index.html"
+  }
+];
